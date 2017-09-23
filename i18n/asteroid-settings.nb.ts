d694a0b97475a0cef0b51174fac0ab1bddedf6db
Allan Nordhøy <epost@anotheragency.no>
2017-09-23 08:24:05 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (34 of 34 strings)

--- i18n/asteroid-settings.nb.ts
+++ i18n/asteroid-settings.nb.ts
@@ -5,47 +5,47 @@
     <name>BluetoothPage</name>
     <message>
         <source>Use Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Bruk Blåtann</translation>
     </message>
     <message>
         <source>Sync notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Synkroniseringsmerknader</translation>
     </message>
     <message>
         <source>Sync time</source>
-        <translation type="unfinished"></translation>
+        <translation>Synkroniser tid</translation>
     </message>
     <message>
         <location filename="../BluetoothPage.qml" line="50"/>
         <source>Bluetooth on</source>
-        <translation type="unfinished"></translation>
+        <translation>Blåtann på</translation>
     </message>
     <message>
         <location filename="../BluetoothPage.qml" line="50"/>
         <source>Bluetooth off</source>
-        <translation type="unfinished"></translation>
+        <translation>Blåtann av</translation>
     </message>
     <message>
         <location filename="../BluetoothPage.qml" line="50"/>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Tilkoblet</translation>
     </message>
     <message>
         <location filename="../BluetoothPage.qml" line="50"/>
         <source>Not Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Frakoblet</translation>
     </message>
 </context>
 <context>
     <name>BrightnessPage</name>
     <message>
         <source>Brightness: %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>Lysstyrke: %1%</translation>
     </message>
     <message>
         <location filename="../BrightnessPage.qml" line="34"/>
         <source>Brightness %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>Lysstyrke %1%</translation>
     </message>
 </context>
 <context>
@@ -53,7 +53,7 @@
     <message>
         <location filename="../DatePage.qml" line="32"/>
         <source>Select a date:</source>
-        <translation type="unfinished"></translation>
+        <translation>Velg en dato:</translation>
     </message>
 </context>
 <context>
@@ -61,28 +61,28 @@
     <message>
         <location filename="../LanguagePage.qml" line="31"/>
         <source>Select a language:</source>
-        <translation type="unfinished"></translation>
+        <translation>Velg et språk:</translation>
     </message>
     <message>
         <location filename="../LanguagePage.qml" line="104"/>
         <source>Rebooting...</source>
-        <translation type="unfinished"></translation>
+        <translation>Starter på nytt…</translation>
     </message>
 </context>
 <context>
     <name>PoweroffPage</name>
     <message>
         <source>Turn off</source>
-        <translation type="unfinished"></translation>
+        <translation>Skru av</translation>
     </message>
     <message>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>Omstart</translation>
     </message>
     <message>
         <location filename="../PoweroffPage.qml" line="28"/>
         <source>Power off AsteroidOS</source>
-        <translation type="unfinished"></translation>
+        <translation>Skru av AsteroidOS</translation>
     </message>
 </context>
 <context>
@@ -90,7 +90,7 @@
     <message>
         <location filename="asteroid-settings.desktop.h" line="5"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Innstillinger</translation>
     </message>
 </context>
 <context>
@@ -98,7 +98,7 @@
     <message>
         <location filename="../RebootPage.qml" line="28"/>
         <source>Reboot AsteroidOS</source>
-        <translation type="unfinished"></translation>
+        <translation>Start AsteroidOS på nytt</translation>
     </message>
 </context>
 <context>
@@ -106,7 +106,7 @@
     <message>
         <location filename="../TimePage.qml" line="38"/>
         <source>Select a time:</source>
-        <translation type="unfinished"></translation>
+        <translation>Velg en tid:</translation>
     </message>
 </context>
 <context>
@@ -114,27 +114,27 @@
     <message>
         <location filename="../USBPage.qml" line="41"/>
         <source>Adb Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>ADB-modus</translation>
     </message>
     <message>
         <location filename="../USBPage.qml" line="42"/>
         <source>Developer Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Utviklermodus</translation>
     </message>
     <message>
         <location filename="../USBPage.qml" line="43"/>
         <source>Mass Storage</source>
-        <translation type="unfinished"></translation>
+        <translation>Lagring</translation>
     </message>
     <message>
         <location filename="../USBPage.qml" line="44"/>
         <source>MTP Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>MTP-modus</translation>
     </message>
     <message>
         <location filename="../USBPage.qml" line="30"/>
         <source>Select a USB mode:</source>
-        <translation type="unfinished"></translation>
+        <translation>Velg et USB-modus:</translation>
     </message>
 </context>
 <context>
@@ -142,57 +142,57 @@
     <message>
         <location filename="../main.qml" line="70"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Språk</translation>
     </message>
     <message>
         <location filename="../main.qml" line="60"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Tid</translation>
     </message>
     <message>
         <location filename="../main.qml" line="65"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Dato</translation>
     </message>
     <message>
         <location filename="../main.qml" line="75"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Blåtann</translation>
     </message>
     <message>
         <location filename="../main.qml" line="80"/>
         <source>Brightness</source>
-        <translation type="unfinished"></translation>
+        <translation>Lysstyrke</translation>
     </message>
     <message>
         <location filename="../main.qml" line="85"/>
         <source>Wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation>Bakgrunn</translation>
     </message>
     <message>
         <location filename="../main.qml" line="90"/>
         <source>Watchface</source>
-        <translation type="unfinished"></translation>
+        <translation>Klokkeskive</translation>
     </message>
     <message>
         <location filename="../main.qml" line="95"/>
         <source>USB</source>
-        <translation type="unfinished"></translation>
+        <translation>USB</translation>
     </message>
     <message>
         <location filename="../main.qml" line="100"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Skru av</translation>
     </message>
     <message>
         <location filename="../main.qml" line="110"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Om</translation>
     </message>
     <message>
         <location filename="../main.qml" line="105"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Omstart</translation>
     </message>
 </context>
 </TS>

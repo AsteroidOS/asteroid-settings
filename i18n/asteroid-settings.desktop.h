77c040d520ab3f3af454e665de8aead273ff3c56
Florent Revest <revestflo@gmail.com>
2017-11-21 13:45:29 +0100

    desktop.h: Use id based translations

--- i18n/asteroid-settings.desktop.h
+++ i18n/asteroid-settings.desktop.h
@@ -2,4 +2,5 @@
  * will be integrated to i18n/.ts files, translated on Weblate and then merged
  * back with the .desktop.template file into a single .desktop file. */
 
-QObject::tr("Settings");
+//% "Settings"
+qtTrId("id-app-launcher-name");

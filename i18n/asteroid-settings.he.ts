09dbfc31e3fb95b49234f7b3adb73c213f2d2793
Yaron Shahrabani <sh.yaron@gmail.com>
2017-11-25 13:26:52 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (32 of 32 strings)

--- i18n/asteroid-settings.he.ts
+++ i18n/asteroid-settings.he.ts
@@ -6,107 +6,107 @@
     <message id="id-bluetooth-on">
         <location filename="../BluetoothPage.qml" line="28"/>
         <source>Bluetooth on</source>
-        <translation></translation>
+        <translation>Bluetooth פעיל</translation>
     </message>
     <message id="id-bluetooth-off">
         <location filename="../BluetoothPage.qml" line="30"/>
         <source>Bluetooth off</source>
-        <translation></translation>
+        <translation>Bluetooth כבוי</translation>
     </message>
     <message id="id-connected">
         <location filename="../BluetoothPage.qml" line="32"/>
         <source>Connected</source>
-        <translation></translation>
+        <translation>מחובר</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../BluetoothPage.qml" line="34"/>
         <source>Not connected</source>
-        <translation></translation>
+        <translation>מנותק</translation>
     </message>
     <message id="id-brightness-percentage">
         <location filename="../BrightnessPage.qml" line="36"/>
         <source>Brightness %1%</source>
-        <translation></translation>
+        <translation>בהירות %1%</translation>
     </message>
     <message id="id-date-select">
         <location filename="../DatePage.qml" line="32"/>
         <source>Select a date:</source>
-        <translation></translation>
+        <translation>בחירת תאריך:</translation>
     </message>
     <message id="id-language-select">
         <location filename="../LanguagePage.qml" line="32"/>
         <source>Select a language:</source>
-        <translation></translation>
+        <translation>בחירת שפה:</translation>
     </message>
     <message id="id-rebooting">
         <location filename="../LanguagePage.qml" line="78"/>
         <source>Rebooting...</source>
-        <translation></translation>
+        <translation>הפעלה מחדש...</translation>
     </message>
     <message id="id-poweroff-warn">
         <location filename="../PoweroffPage.qml" line="29"/>
         <source>Power off AsteroidOS</source>
-        <translation></translation>
+        <translation>כיבוי AsteroidOS</translation>
     </message>
     <message id="id-reboot-warn">
         <location filename="../RebootPage.qml" line="29"/>
         <source>Reboot AsteroidOS</source>
-        <translation></translation>
+        <translation>הפעלת AsteroidOS מחדש</translation>
     </message>
     <message id="id-sound-percentage">
         <location filename="../SoundPage.qml" line="36"/>
         <source>Sound %1%</source>
-        <translation></translation>
+        <translation>צליל %1%</translation>
     </message>
     <message id="id-time-select">
         <location filename="../TimePage.qml" line="46"/>
         <source>Select a time:</source>
-        <translation></translation>
+        <translation>בחירת שעה:</translation>
     </message>
     <message id="id-usb-mode-select">
         <location filename="../USBPage.qml" line="30"/>
         <source>Select a USB mode:</source>
-        <translation></translation>
+        <translation>בחירת מצב USB:</translation>
     </message>
     <message id="id-adb-mode">
         <location filename="../USBPage.qml" line="43"/>
         <source>Adb Mode</source>
-        <translation></translation>
+        <translation>מצב Adb</translation>
     </message>
     <message id="id-developer-mode">
         <location filename="../USBPage.qml" line="45"/>
         <source>Developer Mode</source>
-        <translation></translation>
+        <translation>מצב פיתוח</translation>
     </message>
     <message id="id-mtp-mode">
         <location filename="../USBPage.qml" line="47"/>
         <source>MTP Mode</source>
-        <translation></translation>
+        <translation>מצב התקן אחסון</translation>
     </message>
     <message id="id-12h-format">
         <location filename="../UnitsPage.qml" line="43"/>
         <source>Use 12H format:</source>
-        <translation></translation>
+        <translation>תבנית 12 שעות:</translation>
     </message>
     <message id="id-fahrenheit">
         <location filename="../UnitsPage.qml" line="58"/>
         <source>Use Fahrenheit:</source>
-        <translation></translation>
+        <translation>שימוש בפרנהייט:</translation>
     </message>
     <message id="id-time-page">
         <location filename="../main.qml" line="69"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>שעה</translation>
     </message>
     <message id="id-date-page">
         <location filename="../main.qml" line="75"/>
         <source>Date</source>
-        <translation></translation>
+        <translation>תאריך</translation>
     </message>
     <message id="id-language-page">
         <location filename="../main.qml" line="81"/>
         <source>Language</source>
-        <translation></translation>
+        <translation>שפה</translation>
     </message>
     <message id="id-bluetooth-page">
         <location filename="../main.qml" line="87"/>
@@ -116,27 +116,27 @@
     <message id="id-brightness-page">
         <location filename="../main.qml" line="93"/>
         <source>Brightness</source>
-        <translation></translation>
+        <translation>בהירות</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="99"/>
         <source>Sound</source>
-        <translation></translation>
+        <translation>צליל</translation>
     </message>
     <message id="id-units-page">
         <location filename="../main.qml" line="107"/>
         <source>Units</source>
-        <translation></translation>
+        <translation>יחידות</translation>
     </message>
     <message id="id-wallpaper-page">
         <location filename="../main.qml" line="113"/>
         <source>Wallpaper</source>
-        <translation></translation>
+        <translation>תמונת רקע</translation>
     </message>
     <message id="id-watchface-page">
         <location filename="../main.qml" line="119"/>
         <source>Watchface</source>
-        <translation></translation>
+        <translation>פני השעון</translation>
     </message>
     <message id="id-usb-page">
         <location filename="../main.qml" line="125"/>
@@ -146,22 +146,22 @@
     <message id="id-poweroff-page">
         <location filename="../main.qml" line="131"/>
         <source>Power Off</source>
-        <translation></translation>
+        <translation>כיבוי</translation>
     </message>
     <message id="id-reboot-page">
         <location filename="../main.qml" line="137"/>
         <source>Reboot</source>
-        <translation></translation>
+        <translation>הפעלה מחדש</translation>
     </message>
     <message id="id-about-page">
         <location filename="../main.qml" line="143"/>
         <source>About</source>
-        <translation></translation>
+        <translation>על אודות</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-settings.desktop.h" line="6"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>הגדרות</translation>
     </message>
 </context>
 </TS>

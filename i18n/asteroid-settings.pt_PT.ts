344e968daa4c33e424e3d0702f867ac9285790f1
Pedro Silva <pedrenriques@gmail.com>
2017-12-08 03:06:46 +0000

    Translated using Weblate (Portuguese (Portugal))
    Currently translated at 100.0% (28 of 28 strings)

--- i18n/asteroid-settings.pt_PT.ts
+++ i18n/asteroid-settings.pt_PT.ts
@@ -6,42 +6,42 @@
     <message id="id-bluetooth-on">
         <location filename="../BluetoothPage.qml" line="28"/>
         <source>Bluetooth on</source>
-        <translation></translation>
+        <translation>Bluetooth ligado</translation>
     </message>
     <message id="id-bluetooth-off">
         <location filename="../BluetoothPage.qml" line="30"/>
         <source>Bluetooth off</source>
-        <translation></translation>
+        <translation>Bluetooth desligado</translation>
     </message>
     <message id="id-connected">
         <location filename="../BluetoothPage.qml" line="32"/>
         <source>Connected</source>
-        <translation></translation>
+        <translation>Ligado</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../BluetoothPage.qml" line="34"/>
         <source>Not connected</source>
-        <translation></translation>
+        <translation>Desligado</translation>
     </message>
     <message id="id-brightness-percentage">
         <location filename="../BrightnessPage.qml" line="36"/>
         <source>Brightness %1%</source>
-        <translation></translation>
+        <translation>Luminosidade %1%</translation>
     </message>
     <message id="id-rebooting">
         <location filename="../LanguagePage.qml" line="70"/>
         <source>Rebooting...</source>
-        <translation></translation>
+        <translation>A reiniciar...</translation>
     </message>
     <message id="id-poweroff-warn">
         <location filename="../PoweroffPage.qml" line="29"/>
         <source>Power off AsteroidOS</source>
-        <translation></translation>
+        <translation>Desligar AsteroidOS</translation>
     </message>
     <message id="id-reboot-warn">
         <location filename="../RebootPage.qml" line="29"/>
         <source>Reboot AsteroidOS</source>
-        <translation></translation>
+        <translation>Reiniciar AsteroidOS</translation>
     </message>
     <message id="id-sound-percentage">
         <location filename="../SoundPage.qml" line="36"/>
@@ -51,45 +51,45 @@
     <message id="id-adb-mode">
         <location filename="../USBPage.qml" line="35"/>
         <source>Adb Mode</source>
-        <translation></translation>
+        <translation>Modo ADB</translation>
     </message>
     <message id="id-developer-mode">
         <location filename="../USBPage.qml" line="37"/>
         <source>Developer Mode</source>
-        <translation></translation>
+        <translation>Modo Programador</translation>
     </message>
     <message id="id-mtp-mode">
         <location filename="../USBPage.qml" line="39"/>
         <source>MTP Mode</source>
-        <translation></translation>
+        <translation>Modo MTP</translation>
     </message>
     <message id="id-12h-format">
         <location filename="../UnitsPage.qml" line="48"/>
         <source>Use 12H format:</source>
-        <translation></translation>
+        <translation>Utilizar formato de 12 horas:</translation>
     </message>
     <message id="id-fahrenheit">
         <location filename="../UnitsPage.qml" line="63"/>
         <source>Use Fahrenheit:</source>
-        <translation></translation>
+        <translation>Utilizar Fahrenheit:</translation>
     </message>
     <message id="id-time-page">
         <location filename="../TimePage.qml" line="45"/>
         <location filename="../main.qml" line="69"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>Hora</translation>
     </message>
     <message id="id-date-page">
         <location filename="../DatePage.qml" line="31"/>
         <location filename="../main.qml" line="75"/>
         <source>Date</source>
-        <translation></translation>
+        <translation>Data</translation>
     </message>
     <message id="id-language-page">
         <location filename="../LanguagePage.qml" line="31"/>
         <location filename="../main.qml" line="81"/>
         <source>Language</source>
-        <translation></translation>
+        <translation>Idioma</translation>
     </message>
     <message id="id-bluetooth-page">
         <location filename="../main.qml" line="87"/>
@@ -99,28 +99,28 @@
     <message id="id-brightness-page">
         <location filename="../main.qml" line="93"/>
         <source>Brightness</source>
-        <translation></translation>
+        <translation>Luminosidade</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="99"/>
         <source>Sound</source>
-        <translation></translation>
+        <translation>Som</translation>
     </message>
     <message id="id-units-page">
         <location filename="../UnitsPage.qml" line="26"/>
         <location filename="../main.qml" line="107"/>
         <source>Units</source>
-        <translation></translation>
+        <translation>Unidades</translation>
     </message>
     <message id="id-wallpaper-page">
         <location filename="../main.qml" line="113"/>
         <source>Wallpaper</source>
-        <translation></translation>
+        <translation>Imagem de fundo</translation>
     </message>
     <message id="id-watchface-page">
         <location filename="../main.qml" line="119"/>
         <source>Watchface</source>
-        <translation></translation>
+        <translation>Mostrador</translation>
     </message>
     <message id="id-usb-page">
         <location filename="../USBPage.qml" line="29"/>
@@ -131,22 +131,22 @@
     <message id="id-poweroff-page">
         <location filename="../main.qml" line="131"/>
         <source>Power Off</source>
-        <translation></translation>
+        <translation>Desligar</translation>
     </message>
     <message id="id-reboot-page">
         <location filename="../main.qml" line="137"/>
         <source>Reboot</source>
-        <translation></translation>
+        <translation>Reiniciar</translation>
     </message>
     <message id="id-about-page">
         <location filename="../main.qml" line="143"/>
         <source>About</source>
-        <translation></translation>
+        <translation>Sobre</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-settings.desktop.h" line="6"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>Definições</translation>
     </message>
 </context>
 </TS>

22c9c562b5bd60d91eba9aa4b0ff2c22018d19c0
Joel Vinay Kumar <lavotivinay@gmail.com>
2018-02-01 12:11:25 +0000

    Translated using Weblate (Telugu)
    Currently translated at 100.0% (28 of 28 strings)

--- i18n/asteroid-settings.te.ts
+++ i18n/asteroid-settings.te.ts
@@ -6,147 +6,147 @@
     <message id="id-bluetooth-on">
         <location filename="../BluetoothPage.qml" line="28"/>
         <source>Bluetooth on</source>
-        <translation></translation>
+        <translation>బ్లూటూత్ ఆన్</translation>
     </message>
     <message id="id-bluetooth-off">
         <location filename="../BluetoothPage.qml" line="30"/>
         <source>Bluetooth off</source>
-        <translation></translation>
+        <translation>బ్లూటూత్ ఆఫ్</translation>
     </message>
     <message id="id-connected">
         <location filename="../BluetoothPage.qml" line="32"/>
         <source>Connected</source>
-        <translation></translation>
+        <translation>కనెక్టెడ్</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../BluetoothPage.qml" line="34"/>
         <source>Not connected</source>
-        <translation></translation>
+        <translation>కనెక్టెడ్ లేదు</translation>
     </message>
     <message id="id-brightness-percentage">
         <location filename="../BrightnessPage.qml" line="36"/>
         <source>Brightness %1%</source>
-        <translation></translation>
+        <translation>ప్రకాశవంతత %1%</translation>
     </message>
     <message id="id-rebooting">
         <location filename="../LanguagePage.qml" line="70"/>
         <source>Rebooting...</source>
-        <translation></translation>
+        <translation>రీబూట్ చేస్తున్నాం...</translation>
     </message>
     <message id="id-poweroff-warn">
         <location filename="../PoweroffPage.qml" line="29"/>
         <source>Power off AsteroidOS</source>
-        <translation></translation>
+        <translation>ఆస్టిరోఇడ్ OS ని ఆపివేయండి</translation>
     </message>
     <message id="id-reboot-warn">
         <location filename="../RebootPage.qml" line="29"/>
         <source>Reboot AsteroidOS</source>
-        <translation></translation>
+        <translation>ఆస్టిరోఇడ్ OS ని రీబూట్ చేయి</translation>
     </message>
     <message id="id-sound-percentage">
         <location filename="../SoundPage.qml" line="36"/>
         <source>Volume %1%</source>
-        <translation></translation>
+        <translation>శబ్దం %1%</translation>
     </message>
     <message id="id-adb-mode">
         <location filename="../USBPage.qml" line="35"/>
         <source>Adb Mode</source>
-        <translation></translation>
+        <translation>ఎడిబి మోడ్</translation>
     </message>
     <message id="id-developer-mode">
         <location filename="../USBPage.qml" line="37"/>
         <source>Developer Mode</source>
-        <translation></translation>
+        <translation>డెవలపర్ మోడ్</translation>
     </message>
     <message id="id-mtp-mode">
         <location filename="../USBPage.qml" line="39"/>
         <source>MTP Mode</source>
-        <translation></translation>
+        <translation>ఎంటిపి మోడ్</translation>
     </message>
     <message id="id-12h-format">
         <location filename="../UnitsPage.qml" line="48"/>
         <source>Use 12H format:</source>
-        <translation></translation>
+        <translation>12H ఫార్మాట్ ను వాడు:</translation>
     </message>
     <message id="id-fahrenheit">
         <location filename="../UnitsPage.qml" line="63"/>
         <source>Use Fahrenheit:</source>
-        <translation></translation>
+        <translation>ఫరెన్హెఇట్ ను వాడు:</translation>
     </message>
     <message id="id-time-page">
         <location filename="../TimePage.qml" line="45"/>
         <location filename="../main.qml" line="69"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>సమయం</translation>
     </message>
     <message id="id-date-page">
         <location filename="../DatePage.qml" line="31"/>
         <location filename="../main.qml" line="75"/>
         <source>Date</source>
-        <translation></translation>
+        <translation>తేది</translation>
     </message>
     <message id="id-language-page">
         <location filename="../LanguagePage.qml" line="31"/>
         <location filename="../main.qml" line="81"/>
         <source>Language</source>
-        <translation></translation>
+        <translation>భాష</translation>
     </message>
     <message id="id-bluetooth-page">
         <location filename="../main.qml" line="87"/>
         <source>Bluetooth</source>
-        <translation></translation>
+        <translation>బ్లూటూత్</translation>
     </message>
     <message id="id-brightness-page">
         <location filename="../main.qml" line="93"/>
         <source>Brightness</source>
-        <translation></translation>
+        <translation>ప్రకాశవంతత</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="99"/>
         <source>Sound</source>
-        <translation></translation>
+        <translation>శబ్దం</translation>
     </message>
     <message id="id-units-page">
         <location filename="../UnitsPage.qml" line="26"/>
         <location filename="../main.qml" line="107"/>
         <source>Units</source>
-        <translation></translation>
+        <translation>యూనిట్స్</translation>
     </message>
     <message id="id-wallpaper-page">
         <location filename="../main.qml" line="113"/>
         <source>Wallpaper</source>
-        <translation></translation>
+        <translation>వాల్పేపర్</translation>
     </message>
     <message id="id-watchface-page">
         <location filename="../main.qml" line="119"/>
         <source>Watchface</source>
-        <translation></translation>
+        <translation>గడియారముఖం</translation>
     </message>
     <message id="id-usb-page">
         <location filename="../USBPage.qml" line="29"/>
         <location filename="../main.qml" line="125"/>
         <source>USB</source>
-        <translation></translation>
+        <translation>యుఎస్బి</translation>
     </message>
     <message id="id-poweroff-page">
         <location filename="../main.qml" line="131"/>
         <source>Power Off</source>
-        <translation></translation>
+        <translation>మూసివేయి</translation>
     </message>
     <message id="id-reboot-page">
         <location filename="../main.qml" line="137"/>
         <source>Reboot</source>
-        <translation></translation>
+        <translation>రీబూట్</translation>
     </message>
     <message id="id-about-page">
         <location filename="../main.qml" line="143"/>
         <source>About</source>
-        <translation></translation>
+        <translation>గురించి</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-settings.desktop.h" line="6"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>సెట్టింగ్స్</translation>
     </message>
 </context>
 </TS>

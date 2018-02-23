bf880faecbfa13829a64f999ffde15f5a4a1e516
sadraldin rastegar <s_qwerty13@live.com>
2018-02-23 11:10:11 +0000

    Translated using Weblate (Persian)
    Currently translated at 96.4% (27 of 28 strings)
    
    it's not good to say "Power off AsteroidOS". but it's good to say"Power off Device"...

--- i18n/asteroid-settings.fa.ts
+++ i18n/asteroid-settings.fa.ts
@@ -31,17 +31,17 @@
     <message id="id-poweroff-warn">
         <location filename="../PoweroffPage.qml" line="29"/>
         <source>Power off AsteroidOS</source>
-        <translation type="unfinished"></translation>
+        <translation>خاموش کردن دستگاه</translation>
     </message>
     <message id="id-reboot-warn">
         <location filename="../RebootPage.qml" line="29"/>
         <source>Reboot AsteroidOS</source>
-        <translation type="unfinished"></translation>
+        <translation>راه‌اندازی مجدد دستگاه</translation>
     </message>
     <message id="id-sound-percentage">
         <location filename="../SoundPage.qml" line="36"/>
         <source>Volume %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>میزان صدا %1%</translation>
     </message>
     <message id="id-charging-only">
         <location filename="../USBPage.qml" line="35"/>
@@ -66,12 +66,12 @@
     <message id="id-12h-format">
         <location filename="../UnitsPage.qml" line="48"/>
         <source>Use 12H format:</source>
-        <translation type="unfinished"></translation>
+        <translation>استفاده از فرمت 12ساعته:</translation>
     </message>
     <message id="id-fahrenheit">
         <location filename="../UnitsPage.qml" line="63"/>
         <source>Use Fahrenheit:</source>
-        <translation type="unfinished"></translation>
+        <translation>استفاده از فارنهایت:</translation>
     </message>
     <message id="id-time-page">
         <location filename="../TimePage.qml" line="40"/>
@@ -104,13 +104,13 @@
     <message id="id-sound-page">
         <location filename="../main.qml" line="99"/>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>صدا</translation>
     </message>
     <message id="id-units-page">
         <location filename="../UnitsPage.qml" line="26"/>
         <location filename="../main.qml" line="107"/>
         <source>Units</source>
-        <translation type="unfinished"></translation>
+        <translation>واحد ها</translation>
     </message>
     <message id="id-wallpaper-page">
         <location filename="../main.qml" line="113"/>
@@ -136,7 +136,7 @@
     <message id="id-reboot-page">
         <location filename="../main.qml" line="137"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>راه‌اندازی مجدد</translation>
     </message>
     <message id="id-about-page">
         <location filename="../main.qml" line="143"/>

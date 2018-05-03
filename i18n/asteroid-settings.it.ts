6802d6f473bfd173918b9b80997eeeaa9c6adb18
Ale-Ma <alemassimiani@gmail.com>
2018-05-03 21:36:48 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (28 of 28 strings)

--- i18n/asteroid-settings.it.ts
+++ i18n/asteroid-settings.it.ts
@@ -113,7 +113,7 @@
     </message>
     <message id="id-charging-only">
         <source>Charging only</source>
-        <translation type="unfinished"></translation>
+        <translation>Solo carica</translation>
     </message>
 </context>
 </TS>

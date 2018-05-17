459b3c11a7259c092c77a83016a0812cafafbb37
ellan <ellan123200@gmail.com>
2018-05-17 15:52:54 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (28 of 28 strings)

--- i18n/asteroid-settings.zh_Hant.ts
+++ i18n/asteroid-settings.zh_Hant.ts
@@ -6,121 +6,121 @@
     <message id="id-bluetooth-on">
         <location filename="../BluetoothPage.qml" line="28"/>
         <source>Bluetooth on</source>
-        <translation></translation>
+        <translation>藍牙已開啟</translation>
     </message>
     <message id="id-bluetooth-off">
         <location filename="../BluetoothPage.qml" line="30"/>
         <source>Bluetooth off</source>
-        <translation></translation>
+        <translation>藍牙已關閉</translation>
     </message>
     <message id="id-connected">
         <location filename="../BluetoothPage.qml" line="32"/>
         <source>Connected</source>
-        <translation></translation>
+        <translation>已連線</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../BluetoothPage.qml" line="34"/>
         <source>Not connected</source>
-        <translation></translation>
+        <translation>未連線</translation>
     </message>
     <message id="id-brightness-percentage">
         <location filename="../BrightnessPage.qml" line="36"/>
         <source>Brightness %1%</source>
-        <translation></translation>
+        <translation>亮度 %1%</translation>
     </message>
     <message id="id-poweroff-warn">
         <location filename="../PoweroffPage.qml" line="29"/>
         <source>Power off AsteroidOS</source>
-        <translation></translation>
+        <translation>關閉 AsteroidOS</translation>
     </message>
     <message id="id-reboot-warn">
         <location filename="../RebootPage.qml" line="29"/>
         <source>Reboot AsteroidOS</source>
-        <translation></translation>
+        <translation>重新啟動 AsteroidOS</translation>
     </message>
     <message id="id-sound-percentage">
         <location filename="../SoundPage.qml" line="36"/>
         <source>Volume %1%</source>
-        <translation></translation>
+        <translation>音量 %1%</translation>
     </message>
     <message id="id-charging-only">
         <location filename="../USBPage.qml" line="35"/>
         <source>Charging only</source>
-        <translation></translation>
+        <translation>僅充電</translation>
     </message>
     <message id="id-adb-mode">
         <location filename="../USBPage.qml" line="37"/>
         <source>ADB Mode</source>
-        <translation></translation>
+        <translation>ADB 模式</translation>
     </message>
     <message id="id-developer-mode">
         <location filename="../USBPage.qml" line="39"/>
         <source>Developer Mode</source>
-        <translation></translation>
+        <translation>開發者模式</translation>
     </message>
     <message id="id-mtp-mode">
         <location filename="../USBPage.qml" line="41"/>
         <source>MTP Mode</source>
-        <translation></translation>
+        <translation>MTP 模式</translation>
     </message>
     <message id="id-12h-format">
         <location filename="../UnitsPage.qml" line="48"/>
         <source>Use 12H format:</source>
-        <translation></translation>
+        <translation>使用12小時制:</translation>
     </message>
     <message id="id-fahrenheit">
         <location filename="../UnitsPage.qml" line="63"/>
         <source>Use Fahrenheit:</source>
-        <translation></translation>
+        <translation>使用華氏溫標:</translation>
     </message>
     <message id="id-time-page">
         <location filename="../TimePage.qml" line="40"/>
         <location filename="../main.qml" line="69"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>時間</translation>
     </message>
     <message id="id-date-page">
         <location filename="../DatePage.qml" line="31"/>
         <location filename="../main.qml" line="75"/>
         <source>Date</source>
-        <translation></translation>
+        <translation>日期</translation>
     </message>
     <message id="id-language-page">
         <location filename="../LanguagePage.qml" line="31"/>
         <location filename="../main.qml" line="81"/>
         <source>Language</source>
-        <translation></translation>
+        <translation>語言</translation>
     </message>
     <message id="id-bluetooth-page">
         <location filename="../main.qml" line="87"/>
         <source>Bluetooth</source>
-        <translation></translation>
+        <translation>藍牙</translation>
     </message>
     <message id="id-brightness-page">
         <location filename="../main.qml" line="93"/>
         <source>Brightness</source>
-        <translation></translation>
+        <translation>亮度</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="99"/>
         <source>Sound</source>
-        <translation></translation>
+        <translation>音效</translation>
     </message>
     <message id="id-units-page">
         <location filename="../UnitsPage.qml" line="26"/>
         <location filename="../main.qml" line="107"/>
         <source>Units</source>
-        <translation></translation>
+        <translation>單位</translation>
     </message>
     <message id="id-wallpaper-page">
         <location filename="../main.qml" line="113"/>
         <source>Wallpaper</source>
-        <translation></translation>
+        <translation>桌布</translation>
     </message>
     <message id="id-watchface-page">
         <location filename="../main.qml" line="119"/>
         <source>Watchface</source>
-        <translation></translation>
+        <translation>錶面</translation>
     </message>
     <message id="id-usb-page">
         <location filename="../USBPage.qml" line="29"/>
@@ -131,22 +131,22 @@
     <message id="id-poweroff-page">
         <location filename="../main.qml" line="131"/>
         <source>Power Off</source>
-        <translation></translation>
+        <translation>關機</translation>
     </message>
     <message id="id-reboot-page">
         <location filename="../main.qml" line="137"/>
         <source>Reboot</source>
-        <translation></translation>
+        <translation>重新啟動</translation>
     </message>
     <message id="id-about-page">
         <location filename="../main.qml" line="143"/>
         <source>About</source>
-        <translation></translation>
+        <translation>關於</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-settings.desktop.h" line="6"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>設定</translation>
     </message>
 </context>
 </TS>

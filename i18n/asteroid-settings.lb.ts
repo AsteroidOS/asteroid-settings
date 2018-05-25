210dda6f14c0bb5734af83148e1fc12cb57c1f45
Mika Beckerich <beckerichmika@gmail.com>
2018-05-25 15:43:48 +0000

    Translated using Weblate (Luxembourgish)
    Currently translated at 100.0% (28 of 28 strings)

--- i18n/asteroid-settings.lb.ts
+++ i18n/asteroid-settings.lb.ts
@@ -11,42 +11,42 @@
     <message id="id-bluetooth-off">
         <location filename="../BluetoothPage.qml" line="30"/>
         <source>Bluetooth off</source>
-        <translation></translation>
+        <translation>Bluetooth aus</translation>
     </message>
     <message id="id-connected">
         <location filename="../BluetoothPage.qml" line="32"/>
         <source>Connected</source>
-        <translation></translation>
+        <translation>Verbonnen</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../BluetoothPage.qml" line="34"/>
         <source>Not connected</source>
-        <translation></translation>
+        <translation>Net verbonnen</translation>
     </message>
     <message id="id-brightness-percentage">
         <location filename="../BrightnessPage.qml" line="36"/>
         <source>Brightness %1%</source>
-        <translation></translation>
+        <translation>Hellegkeet %1%</translation>
     </message>
     <message id="id-poweroff-warn">
         <location filename="../PoweroffPage.qml" line="29"/>
         <source>Power off AsteroidOS</source>
-        <translation></translation>
+        <translation>AsteroidOS ausschalten</translation>
     </message>
     <message id="id-reboot-warn">
         <location filename="../RebootPage.qml" line="29"/>
         <source>Reboot AsteroidOS</source>
-        <translation></translation>
+        <translation>AsteroidOS nei starten</translation>
     </message>
     <message id="id-sound-percentage">
         <location filename="../SoundPage.qml" line="36"/>
         <source>Volume %1%</source>
-        <translation></translation>
+        <translation>Lautstäerkt %1%</translation>
     </message>
     <message id="id-charging-only">
         <location filename="../USBPage.qml" line="35"/>
         <source>Charging only</source>
-        <translation></translation>
+        <translation>Nëmmen oplueden</translation>
     </message>
     <message id="id-adb-mode">
         <location filename="../USBPage.qml" line="37"/>
@@ -61,35 +61,35 @@
     <message id="id-mtp-mode">
         <location filename="../USBPage.qml" line="41"/>
         <source>MTP Mode</source>
-        <translation></translation>
+        <translation>MTP-Modus</translation>
     </message>
     <message id="id-12h-format">
         <location filename="../UnitsPage.qml" line="48"/>
         <source>Use 12H format:</source>
-        <translation></translation>
+        <translation>12-Stonnen-Format:</translation>
     </message>
     <message id="id-fahrenheit">
         <location filename="../UnitsPage.qml" line="63"/>
         <source>Use Fahrenheit:</source>
-        <translation></translation>
+        <translation>Fahrenheit:</translation>
     </message>
     <message id="id-time-page">
         <location filename="../TimePage.qml" line="40"/>
         <location filename="../main.qml" line="69"/>
         <source>Time</source>
-        <translation></translation>
+        <translation>Auerzäit</translation>
     </message>
     <message id="id-date-page">
         <location filename="../DatePage.qml" line="31"/>
         <location filename="../main.qml" line="75"/>
         <source>Date</source>
-        <translation></translation>
+        <translation>Datum</translation>
     </message>
     <message id="id-language-page">
         <location filename="../LanguagePage.qml" line="31"/>
         <location filename="../main.qml" line="81"/>
         <source>Language</source>
-        <translation></translation>
+        <translation>Sprooch</translation>
     </message>
     <message id="id-bluetooth-page">
         <location filename="../main.qml" line="87"/>
@@ -99,7 +99,7 @@
     <message id="id-brightness-page">
         <location filename="../main.qml" line="93"/>
         <source>Brightness</source>
-        <translation></translation>
+        <translation>Hellegkeet</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="99"/>
@@ -110,17 +110,17 @@
         <location filename="../UnitsPage.qml" line="26"/>
         <location filename="../main.qml" line="107"/>
         <source>Units</source>
-        <translation></translation>
+        <translation>Unitéiten</translation>
     </message>
     <message id="id-wallpaper-page">
         <location filename="../main.qml" line="113"/>
         <source>Wallpaper</source>
-        <translation></translation>
+        <translation>Hannergrond</translation>
     </message>
     <message id="id-watchface-page">
         <location filename="../main.qml" line="119"/>
         <source>Watchface</source>
-        <translation></translation>
+        <translation>Zifferblat</translation>
     </message>
     <message id="id-usb-page">
         <location filename="../USBPage.qml" line="29"/>
@@ -131,22 +131,22 @@
     <message id="id-poweroff-page">
         <location filename="../main.qml" line="131"/>
         <source>Power Off</source>
-        <translation></translation>
+        <translation>Ausschalten</translation>
     </message>
     <message id="id-reboot-page">
         <location filename="../main.qml" line="137"/>
         <source>Reboot</source>
-        <translation></translation>
+        <translation>Neistart</translation>
     </message>
     <message id="id-about-page">
         <location filename="../main.qml" line="143"/>
         <source>About</source>
-        <translation></translation>
+        <translation>Iwwer</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-settings.desktop.h" line="6"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>Astellungen</translation>
     </message>
 </context>
 </TS>

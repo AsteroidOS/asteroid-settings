b36e649284aba62eb4082af5fff02377135b0059
Dominik Danelski <dominik+foss@danelski.pl>
2018-06-19 20:31:03 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (28 of 28 strings)

--- i18n/asteroid-settings.pl.ts
+++ i18n/asteroid-settings.pl.ts
@@ -16,7 +16,7 @@
     <message id="id-connected">
         <location filename="../BluetoothPage.qml" line="32"/>
         <source>Connected</source>
-        <translation>Połączony</translation>
+        <translation>Połączono</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../BluetoothPage.qml" line="34"/>

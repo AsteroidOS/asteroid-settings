2d74b9151f059659432656d7a4abb4f0c8842da2
mohamad farid <mfarid.ahamid@gmail.com>
2018-08-27 04:20:49 +0000

    Added translation using Weblate (Malay)

---
+++ i18n/asteroid-settings.ms.ts
@@ -0,0 +1,152 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ms">
+<context>
+    <name></name>
+    <message id="id-bluetooth-on">
+        <location filename="../BluetoothPage.qml" line="28"/>
+        <source>Bluetooth on</source>
+        <translation></translation>
+    </message>
+    <message id="id-bluetooth-off">
+        <location filename="../BluetoothPage.qml" line="30"/>
+        <source>Bluetooth off</source>
+        <translation></translation>
+    </message>
+    <message id="id-connected">
+        <location filename="../BluetoothPage.qml" line="32"/>
+        <source>Connected</source>
+        <translation></translation>
+    </message>
+    <message id="id-disconnected">
+        <location filename="../BluetoothPage.qml" line="34"/>
+        <source>Not connected</source>
+        <translation></translation>
+    </message>
+    <message id="id-brightness-percentage">
+        <location filename="../BrightnessPage.qml" line="36"/>
+        <source>Brightness %1%</source>
+        <translation></translation>
+    </message>
+    <message id="id-poweroff-warn">
+        <location filename="../PoweroffPage.qml" line="29"/>
+        <source>Power off AsteroidOS</source>
+        <translation></translation>
+    </message>
+    <message id="id-reboot-warn">
+        <location filename="../RebootPage.qml" line="29"/>
+        <source>Reboot AsteroidOS</source>
+        <translation></translation>
+    </message>
+    <message id="id-sound-percentage">
+        <location filename="../SoundPage.qml" line="36"/>
+        <source>Volume %1%</source>
+        <translation></translation>
+    </message>
+    <message id="id-charging-only">
+        <location filename="../USBPage.qml" line="35"/>
+        <source>Charging only</source>
+        <translation></translation>
+    </message>
+    <message id="id-adb-mode">
+        <location filename="../USBPage.qml" line="37"/>
+        <source>ADB Mode</source>
+        <translation></translation>
+    </message>
+    <message id="id-developer-mode">
+        <location filename="../USBPage.qml" line="39"/>
+        <source>Developer Mode</source>
+        <translation></translation>
+    </message>
+    <message id="id-mtp-mode">
+        <location filename="../USBPage.qml" line="41"/>
+        <source>MTP Mode</source>
+        <translation></translation>
+    </message>
+    <message id="id-12h-format">
+        <location filename="../UnitsPage.qml" line="48"/>
+        <source>Use 12H format:</source>
+        <translation></translation>
+    </message>
+    <message id="id-fahrenheit">
+        <location filename="../UnitsPage.qml" line="63"/>
+        <source>Use Fahrenheit:</source>
+        <translation></translation>
+    </message>
+    <message id="id-time-page">
+        <location filename="../TimePage.qml" line="40"/>
+        <location filename="../main.qml" line="69"/>
+        <source>Time</source>
+        <translation></translation>
+    </message>
+    <message id="id-date-page">
+        <location filename="../DatePage.qml" line="31"/>
+        <location filename="../main.qml" line="75"/>
+        <source>Date</source>
+        <translation></translation>
+    </message>
+    <message id="id-language-page">
+        <location filename="../LanguagePage.qml" line="31"/>
+        <location filename="../main.qml" line="81"/>
+        <source>Language</source>
+        <translation></translation>
+    </message>
+    <message id="id-bluetooth-page">
+        <location filename="../main.qml" line="87"/>
+        <source>Bluetooth</source>
+        <translation></translation>
+    </message>
+    <message id="id-brightness-page">
+        <location filename="../main.qml" line="93"/>
+        <source>Brightness</source>
+        <translation></translation>
+    </message>
+    <message id="id-sound-page">
+        <location filename="../main.qml" line="99"/>
+        <source>Sound</source>
+        <translation></translation>
+    </message>
+    <message id="id-units-page">
+        <location filename="../UnitsPage.qml" line="26"/>
+        <location filename="../main.qml" line="107"/>
+        <source>Units</source>
+        <translation></translation>
+    </message>
+    <message id="id-wallpaper-page">
+        <location filename="../main.qml" line="113"/>
+        <source>Wallpaper</source>
+        <translation></translation>
+    </message>
+    <message id="id-watchface-page">
+        <location filename="../main.qml" line="119"/>
+        <source>Watchface</source>
+        <translation></translation>
+    </message>
+    <message id="id-usb-page">
+        <location filename="../USBPage.qml" line="29"/>
+        <location filename="../main.qml" line="125"/>
+        <source>USB</source>
+        <translation></translation>
+    </message>
+    <message id="id-poweroff-page">
+        <location filename="../main.qml" line="131"/>
+        <source>Power Off</source>
+        <translation></translation>
+    </message>
+    <message id="id-reboot-page">
+        <location filename="../main.qml" line="137"/>
+        <source>Reboot</source>
+        <translation></translation>
+    </message>
+    <message id="id-about-page">
+        <location filename="../main.qml" line="143"/>
+        <source>About</source>
+        <translation></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-settings.desktop.h" line="6"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>

91ddbf1144f83d363bcf091d244c195025fcac27
Jörgen Sjöbom <josjo67@gmail.com>
2020-05-27 23:19:25 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (31 of 31 strings)

--- i18n/asteroid-settings.sv.ts
+++ i18n/asteroid-settings.sv.ts
@@ -99,7 +99,7 @@
     <message id="id-tilt-to-wake-page">
         <location filename="../main.qml" line="98"/>
         <source>Tilt-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>Skaka för att aktivera</translation>
     </message>
     <message id="id-brightness-page">
         <location filename="../main.qml" line="106"/>
@@ -156,12 +156,12 @@
     <message id="id-tilt-to-wake-on">
         <location filename="../TiltToWakePage.qml" line="29"/>
         <source>Tilt-to-wake on</source>
-        <translation type="unfinished"></translation>
+        <translation>Skaka för att aktivera på</translation>
     </message>
     <message id="id-tilt-to-wake-off">
         <location filename="../TiltToWakePage.qml" line="31"/>
         <source>Tilt-to-wake off</source>
-        <translation type="unfinished"></translation>
+        <translation>Skaka för att inaktivera</translation>
     </message>
 </context>
 </TS>

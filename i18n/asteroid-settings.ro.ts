1e9acf7dacb7b45ef47757f8907d842bae536085
Sebastian <seby.nk91@gmail.com>
2020-06-03 11:13:35 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (31 of 31 strings)

--- i18n/asteroid-settings.ro.ts
+++ i18n/asteroid-settings.ro.ts
@@ -6,162 +6,162 @@
     <message id="id-bluetooth-on">
         <location filename="../BluetoothPage.qml" line="28"/>
         <source>Bluetooth on</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth pornit</translation>
     </message>
     <message id="id-bluetooth-off">
         <location filename="../BluetoothPage.qml" line="30"/>
         <source>Bluetooth off</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth oprit</translation>
     </message>
     <message id="id-connected">
         <location filename="../BluetoothPage.qml" line="32"/>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Conectat</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../BluetoothPage.qml" line="34"/>
         <source>Not connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Neconectat</translation>
     </message>
     <message id="id-brightness-percentage">
         <location filename="../BrightnessPage.qml" line="36"/>
         <source>Brightness %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>Luminozitate %1%</translation>
     </message>
     <message id="id-poweroff-warn">
         <location filename="../PoweroffPage.qml" line="29"/>
         <source>Power off AsteroidOS</source>
-        <translation type="unfinished"></translation>
+        <translation>Oprire AsteroidOS</translation>
     </message>
     <message id="id-reboot-warn">
         <location filename="../RebootPage.qml" line="29"/>
         <source>Reboot AsteroidOS</source>
-        <translation type="unfinished"></translation>
+        <translation>Reporniți AsteroidOS</translation>
     </message>
     <message id="id-sound-percentage">
         <location filename="../SoundPage.qml" line="36"/>
         <source>Volume %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>Volum %1%</translation>
     </message>
     <message id="id-charging-only">
         <location filename="../USBPage.qml" line="35"/>
         <source>Charging only</source>
-        <translation type="unfinished"></translation>
+        <translation>Doar încărcare</translation>
     </message>
     <message id="id-adb-mode">
         <location filename="../USBPage.qml" line="37"/>
         <source>ADB Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Mod ADB</translation>
     </message>
     <message id="id-developer-mode">
         <location filename="../USBPage.qml" line="39"/>
         <source>Developer Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Mod Dezvoltator</translation>
     </message>
     <message id="id-mtp-mode">
         <location filename="../USBPage.qml" line="41"/>
         <source>MTP Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Mod MTP</translation>
     </message>
     <message id="id-12h-format">
         <location filename="../UnitsPage.qml" line="48"/>
         <source>Use 12H format:</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilizați formatul 12H:</translation>
     </message>
     <message id="id-fahrenheit">
         <location filename="../UnitsPage.qml" line="63"/>
         <source>Use Fahrenheit:</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilizați Fahrenheit:</translation>
     </message>
     <message id="id-time-page">
         <location filename="../TimePage.qml" line="40"/>
         <location filename="../main.qml" line="74"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Timp</translation>
     </message>
     <message id="id-date-page">
         <location filename="../DatePage.qml" line="31"/>
         <location filename="../main.qml" line="80"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Dată</translation>
     </message>
     <message id="id-language-page">
         <location filename="../LanguagePage.qml" line="31"/>
         <location filename="../main.qml" line="86"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Limbă</translation>
     </message>
     <message id="id-bluetooth-page">
         <location filename="../main.qml" line="92"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth</translation>
     </message>
     <message id="id-tilt-to-wake-page">
         <location filename="../main.qml" line="98"/>
         <source>Tilt-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>Înclinați să se trezească</translation>
     </message>
     <message id="id-brightness-page">
         <location filename="../main.qml" line="106"/>
         <source>Brightness</source>
-        <translation type="unfinished"></translation>
+        <translation>Luminozitate</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="112"/>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>Sunet</translation>
     </message>
     <message id="id-units-page">
         <location filename="../UnitsPage.qml" line="26"/>
         <location filename="../main.qml" line="120"/>
         <source>Units</source>
-        <translation type="unfinished"></translation>
+        <translation>Unităţi</translation>
     </message>
     <message id="id-wallpaper-page">
         <location filename="../main.qml" line="126"/>
         <source>Wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation>Imagine fundal</translation>
     </message>
     <message id="id-watchface-page">
         <location filename="../main.qml" line="132"/>
         <source>Watchface</source>
-        <translation type="unfinished"></translation>
+        <translation>Faţă ceas</translation>
     </message>
     <message id="id-usb-page">
         <location filename="../USBPage.qml" line="29"/>
         <location filename="../main.qml" line="138"/>
         <source>USB</source>
-        <translation type="unfinished"></translation>
+        <translation>USB</translation>
     </message>
     <message id="id-poweroff-page">
         <location filename="../main.qml" line="144"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Oprire</translation>
     </message>
     <message id="id-reboot-page">
         <location filename="../main.qml" line="150"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Repornire</translation>
     </message>
     <message id="id-about-page">
         <location filename="../main.qml" line="156"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Despre</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-settings.desktop.h" line="6"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Setări</translation>
     </message>
     <message id="id-tilt-to-wake-on">
         <location filename="../TiltToWakePage.qml" line="29"/>
         <source>Tilt-to-wake on</source>
-        <translation type="unfinished"></translation>
+        <translation>Înclinați să se trezească</translation>
     </message>
     <message id="id-tilt-to-wake-off">
         <location filename="../TiltToWakePage.qml" line="31"/>
         <source>Tilt-to-wake off</source>
-        <translation type="unfinished"></translation>
+        <translation>Înclinați să se oprească</translation>
     </message>
 </context>
 </TS>

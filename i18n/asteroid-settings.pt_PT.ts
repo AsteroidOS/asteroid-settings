4b9f97546ae90a2065c015c98cd3f18e343a18e5
Manuela Silva <mmsrs@sky.com>
2020-06-17 11:43:43 +0000

    Translated using Weblate (Portuguese (Portugal))
    Currently translated at 100.0% (31 of 31 strings)

--- i18n/asteroid-settings.pt_PT.ts
+++ i18n/asteroid-settings.pt_PT.ts
@@ -99,7 +99,7 @@
     <message id="id-tilt-to-wake-page">
         <location filename="../main.qml" line="98"/>
         <source>Tilt-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>Inclinar para ligar</translation>
     </message>
     <message id="id-brightness-page">
         <location filename="../main.qml" line="106"/>
@@ -156,12 +156,12 @@
     <message id="id-tilt-to-wake-on">
         <location filename="../TiltToWakePage.qml" line="29"/>
         <source>Tilt-to-wake on</source>
-        <translation type="unfinished"></translation>
+        <translation>Inclinar para ligar às</translation>
     </message>
     <message id="id-tilt-to-wake-off">
         <location filename="../TiltToWakePage.qml" line="31"/>
         <source>Tilt-to-wake off</source>
-        <translation type="unfinished"></translation>
+        <translation>Inclinar para desligar</translation>
     </message>
 </context>
 </TS>

e1c336753decaac84a815cb960875429f7f66556
Milo Ivir <mail@milotype.de>
2020-09-09 21:56:31 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (31 of 31 strings)

--- i18n/asteroid-settings.hr.ts
+++ i18n/asteroid-settings.hr.ts
@@ -95,27 +95,27 @@
         <location filename="../DisplayPage.qml" line="33"/>
         <location filename="../main.qml" line="96"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekran</translation>
     </message>
     <message id="id-brightness">
         <location filename="../DisplayPage.qml" line="55"/>
         <source>Brightness</source>
-        <translation type="unfinished">Svjetlina</translation>
+        <translation>Svjetlina</translation>
     </message>
     <message id="id-always-on-display">
         <location filename="../DisplayPage.qml" line="104"/>
         <source>Always on Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Uvijek na ekranu</translation>
     </message>
     <message id="id-tilt-to-wake">
         <location filename="../DisplayPage.qml" line="120"/>
         <source>Tilt-to-wake</source>
-        <translation type="unfinished">Nagni-za-buđenje</translation>
+        <translation>Nagni-za-buđenje</translation>
     </message>
     <message id="id-tap-to-wake">
         <location filename="../DisplayPage.qml" line="139"/>
         <source>Tap-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>Dodirni-za-buđenje</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="102"/>

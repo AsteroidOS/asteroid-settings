392c17d81c421ce1bb494e4642d84e19227774a3
Nikita Epifanov <nikgreens@protonmail.com>
2020-09-09 15:35:55 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (31 of 31 strings)

--- i18n/asteroid-settings.ru.ts
+++ i18n/asteroid-settings.ru.ts
@@ -95,27 +95,27 @@
         <location filename="../DisplayPage.qml" line="33"/>
         <location filename="../main.qml" line="96"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Дисплей</translation>
     </message>
     <message id="id-brightness">
         <location filename="../DisplayPage.qml" line="55"/>
         <source>Brightness</source>
-        <translation type="unfinished">Яркость</translation>
+        <translation>Яркость</translation>
     </message>
     <message id="id-always-on-display">
         <location filename="../DisplayPage.qml" line="104"/>
         <source>Always on Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Always on Display</translation>
     </message>
     <message id="id-tilt-to-wake">
         <location filename="../DisplayPage.qml" line="120"/>
         <source>Tilt-to-wake</source>
-        <translation type="unfinished">Пробуждение по наклону</translation>
+        <translation>Пробуждение по наклону</translation>
     </message>
     <message id="id-tap-to-wake">
         <location filename="../DisplayPage.qml" line="139"/>
         <source>Tap-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>Пробуждение по касанию</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="102"/>

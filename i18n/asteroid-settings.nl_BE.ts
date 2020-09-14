0e2844fd35aab6e0fca4480844078446784a65a9
Issa1553 <fairfull.playing@gmail.com>
2020-09-14 12:32:44 +0000

    Translated using Weblate (Dutch (Belgium))
    Currently translated at 100.0% (31 of 31 strings)

--- i18n/asteroid-settings.nl_BE.ts
+++ i18n/asteroid-settings.nl_BE.ts
@@ -95,27 +95,27 @@
         <location filename="../DisplayPage.qml" line="33"/>
         <location filename="../main.qml" line="96"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Scherm</translation>
     </message>
     <message id="id-brightness">
         <location filename="../DisplayPage.qml" line="55"/>
         <source>Brightness</source>
-        <translation type="unfinished">Helderheid</translation>
+        <translation>Helderheid</translation>
     </message>
     <message id="id-always-on-display">
         <location filename="../DisplayPage.qml" line="104"/>
         <source>Always on Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Actief scherm</translation>
     </message>
     <message id="id-tilt-to-wake">
         <location filename="../DisplayPage.qml" line="120"/>
         <source>Tilt-to-wake</source>
-        <translation type="unfinished">Kantelen voor activeren</translation>
+        <translation>Kantelen om te ontwaken</translation>
     </message>
     <message id="id-tap-to-wake">
         <location filename="../DisplayPage.qml" line="139"/>
         <source>Tap-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>Drukken om te ontwaken</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="102"/>

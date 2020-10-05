9c2c4600b7dfc49193fb764928021fe71db7f88f
rooban23 <TP045411@mail.apu.edu.my>
2020-10-05 11:06:56 +0000

    Translated using Weblate (Telugu)
    Currently translated at 100.0% (31 of 31 strings)

--- i18n/asteroid-settings.te.ts
+++ i18n/asteroid-settings.te.ts
@@ -95,27 +95,27 @@
         <location filename="../DisplayPage.qml" line="33"/>
         <location filename="../main.qml" line="96"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>ప్రదర్శన</translation>
     </message>
     <message id="id-brightness">
         <location filename="../DisplayPage.qml" line="55"/>
         <source>Brightness</source>
-        <translation type="unfinished">ప్రకాశవంతత</translation>
+        <translation>ప్రకాశం</translation>
     </message>
     <message id="id-always-on-display">
         <location filename="../DisplayPage.qml" line="104"/>
         <source>Always on Display</source>
-        <translation type="unfinished"></translation>
+        <translation>ఎల్లప్పుడూ ప్రదర్శనలో ఉంటుంది</translation>
     </message>
     <message id="id-tilt-to-wake">
         <location filename="../DisplayPage.qml" line="120"/>
         <source>Tilt-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>వంపు నుండి మేల్కొలపండి</translation>
     </message>
     <message id="id-tap-to-wake">
         <location filename="../DisplayPage.qml" line="139"/>
         <source>Tap-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>మేల్కొలపడానికి నొక్కండి</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="102"/>

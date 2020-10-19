dea7f0907f020934b02f8710f4c1b3398e12ab07
mondstern <mondstern@snopyta.org>
2020-10-19 18:41:43 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (31 of 31 strings)

--- i18n/asteroid-settings.fr.ts
+++ i18n/asteroid-settings.fr.ts
@@ -95,27 +95,27 @@
         <location filename="../DisplayPage.qml" line="33"/>
         <location filename="../main.qml" line="96"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Afficher</translation>
     </message>
     <message id="id-brightness">
         <location filename="../DisplayPage.qml" line="55"/>
         <source>Brightness</source>
-        <translation type="unfinished">Luminosité</translation>
+        <translation>Lumière</translation>
     </message>
     <message id="id-always-on-display">
         <location filename="../DisplayPage.qml" line="104"/>
         <source>Always on Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Toujours en vue</translation>
     </message>
     <message id="id-tilt-to-wake">
         <location filename="../DisplayPage.qml" line="120"/>
         <source>Tilt-to-wake</source>
-        <translation type="unfinished">Mode Incliner-pour-allumer</translation>
+        <translation>Incliner-pour-se-réveiller</translation>
     </message>
     <message id="id-tap-to-wake">
         <location filename="../DisplayPage.qml" line="139"/>
         <source>Tap-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>Tapoter-pour-réveiller</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="102"/>

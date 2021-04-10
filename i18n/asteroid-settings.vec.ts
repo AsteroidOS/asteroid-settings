edbd260c9914295f155303acde081102857fc788
Man Rub <manuelrubin11@gmail.com>
2021-04-10 20:42:41 +0000

    Translated using Weblate (Venetian)
    Currently translated at 100.0% (31 of 31 strings)

--- i18n/asteroid-settings.vec.ts
+++ i18n/asteroid-settings.vec.ts
@@ -6,163 +6,163 @@
     <message id="id-bluetooth-on">
         <location filename="../BluetoothPage.qml" line="28"/>
         <source>Bluetooth on</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth ativà</translation>
     </message>
     <message id="id-bluetooth-off">
         <location filename="../BluetoothPage.qml" line="30"/>
         <source>Bluetooth off</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth dezativà</translation>
     </message>
     <message id="id-connected">
         <location filename="../BluetoothPage.qml" line="32"/>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Conetesto</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../BluetoothPage.qml" line="34"/>
         <source>Not connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Desconetesto</translation>
     </message>
     <message id="id-poweroff-warn">
         <location filename="../PoweroffPage.qml" line="29"/>
         <source>Power off AsteroidOS</source>
-        <translation type="unfinished"></translation>
+        <translation>Stua AsteroidOS</translation>
     </message>
     <message id="id-reboot-warn">
         <location filename="../RebootPage.qml" line="29"/>
         <source>Reboot AsteroidOS</source>
-        <translation type="unfinished"></translation>
+        <translation>Re-partir AsteroidOS</translation>
     </message>
     <message id="id-sound-percentage">
         <location filename="../SoundPage.qml" line="36"/>
         <source>Volume %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>Vołume %1 %</translation>
     </message>
     <message id="id-charging-only">
         <location filename="../USBPage.qml" line="35"/>
         <source>Charging only</source>
-        <translation type="unfinished"></translation>
+        <translation>Soło carga</translation>
     </message>
     <message id="id-adb-mode">
         <location filename="../USBPage.qml" line="37"/>
         <source>ADB Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Magnera ADB</translation>
     </message>
     <message id="id-developer-mode">
         <location filename="../USBPage.qml" line="39"/>
         <source>Developer Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Magnera desviłupador</translation>
     </message>
     <message id="id-mtp-mode">
         <location filename="../USBPage.qml" line="41"/>
         <source>MTP Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Magnera MTP</translation>
     </message>
     <message id="id-12h-format">
         <location filename="../UnitsPage.qml" line="48"/>
         <source>Use 12H format:</source>
-        <translation type="unfinished"></translation>
+        <translation>Dòpara forma 12 h:</translation>
     </message>
     <message id="id-fahrenheit">
         <location filename="../UnitsPage.qml" line="63"/>
         <source>Use Fahrenheit:</source>
-        <translation type="unfinished"></translation>
+        <translation>Dòpara Fahrenheit:</translation>
     </message>
     <message id="id-time-page">
         <location filename="../TimePage.qml" line="40"/>
         <location filename="../main.qml" line="72"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Ora</translation>
     </message>
     <message id="id-date-page">
         <location filename="../DatePage.qml" line="31"/>
         <location filename="../main.qml" line="78"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Data</translation>
     </message>
     <message id="id-language-page">
         <location filename="../LanguagePage.qml" line="31"/>
         <location filename="../main.qml" line="84"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Łéngua</translation>
     </message>
     <message id="id-bluetooth-page">
         <location filename="../main.qml" line="90"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth</translation>
     </message>
     <message id="id-display-page">
         <location filename="../DisplayPage.qml" line="33"/>
         <location filename="../main.qml" line="96"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Schermo</translation>
     </message>
     <message id="id-brightness">
         <location filename="../DisplayPage.qml" line="55"/>
         <source>Brightness</source>
-        <translation type="unfinished"></translation>
+        <translation>Lumenozità</translation>
     </message>
     <message id="id-always-on-display">
         <location filename="../DisplayPage.qml" line="104"/>
         <source>Always on Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Schermo senpre inpisà</translation>
     </message>
     <message id="id-tilt-to-wake">
         <location filename="../DisplayPage.qml" line="120"/>
         <source>Tilt-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>Inclinar par svejarse</translation>
     </message>
     <message id="id-tap-to-wake">
         <location filename="../DisplayPage.qml" line="139"/>
         <source>Tap-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>Struca par inpisar</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../main.qml" line="102"/>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>Sono</translation>
     </message>
     <message id="id-units-page">
         <location filename="../UnitsPage.qml" line="26"/>
         <location filename="../main.qml" line="110"/>
         <source>Units</source>
-        <translation type="unfinished"></translation>
+        <translation>Unità</translation>
     </message>
     <message id="id-wallpaper-page">
         <location filename="../main.qml" line="116"/>
         <source>Wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation>Sfondo</translation>
     </message>
     <message id="id-watchface-page">
         <location filename="../main.qml" line="122"/>
         <source>Watchface</source>
-        <translation type="unfinished"></translation>
+        <translation>Recuadro</translation>
     </message>
     <message id="id-usb-page">
         <location filename="../USBPage.qml" line="29"/>
         <location filename="../main.qml" line="128"/>
         <source>USB</source>
-        <translation type="unfinished"></translation>
+        <translation>USB</translation>
     </message>
     <message id="id-poweroff-page">
         <location filename="../main.qml" line="134"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Stua</translation>
     </message>
     <message id="id-reboot-page">
         <location filename="../main.qml" line="140"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Rinpisar</translation>
     </message>
     <message id="id-about-page">
         <location filename="../main.qml" line="146"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Arente de</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-settings.desktop.h" line="6"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurasion</translation>
     </message>
 </context>
 </TS>

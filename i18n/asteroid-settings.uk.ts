acf019f910dcba06a6634fdc18d60d896e2e1ef9
Dan <denqwerta@gmail.com>
2022-12-14 22:14:26 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (53 of 53 strings)

--- i18n/asteroid-settings.uk.ts
+++ i18n/asteroid-settings.uk.ts
@@ -6,22 +6,22 @@
     <message id="id-bluetooth-on">
         <location filename="../src/qml/BluetoothPage.qml" line="28"/>
         <source>Bluetooth on</source>
-        <translation>Bluetooth увімкнений</translation>
+        <translation>Bluetooth увімк</translation>
     </message>
     <message id="id-bluetooth-off">
         <location filename="../src/qml/BluetoothPage.qml" line="30"/>
         <source>Bluetooth off</source>
-        <translation>Bluetooth вимкнений</translation>
+        <translation>Bluetooth вимк</translation>
     </message>
     <message id="id-connected">
         <location filename="../src/qml/BluetoothPage.qml" line="32"/>
         <source>Connected</source>
-        <translation>З&apos;єднано</translation>
+        <translation>Підключено</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../src/qml/BluetoothPage.qml" line="34"/>
         <source>Not connected</source>
-        <translation>Не з&apos;єднаний</translation>
+        <translation>Не підключено</translation>
     </message>
     <message id="id-poweroff-warn">
         <location filename="../src/qml/PoweroffPage.qml" line="29"/>
@@ -36,12 +36,12 @@
     <message id="id-sound-percentage">
         <location filename="../src/qml/SoundPage.qml" line="36"/>
         <source>Volume %1%</source>
-        <translation>гучність %1%</translation>
+        <translation>Гучність %1%</translation>
     </message>
     <message id="id-charging-only">
         <location filename="../src/qml/USBPage.qml" line="35"/>
         <source>Charging only</source>
-        <translation>Лише заряджання</translation>
+        <translation>Тільки зарядка</translation>
     </message>
     <message id="id-adb-mode">
         <location filename="../src/qml/USBPage.qml" line="37"/>
@@ -61,12 +61,12 @@
     <message id="id-12h-format">
         <location filename="../src/qml/UnitsPage.qml" line="48"/>
         <source>Use 12H format:</source>
-        <translation>застосовувати 12h формат:</translation>
+        <translation>Використовувати 12h формат:</translation>
     </message>
     <message id="id-fahrenheit">
         <location filename="../src/qml/UnitsPage.qml" line="63"/>
         <source>Use Fahrenheit:</source>
-        <translation>застосовувати градус Фаренгейта:</translation>
+        <translation>Використовувати градус Фаренгейта:</translation>
     </message>
     <message id="id-time-page">
         <location filename="../src/qml/main.qml" line="71"/>
@@ -84,7 +84,7 @@
         <location filename="../src/qml/LanguagePage.qml" line="31"/>
         <location filename="../src/qml/main.qml" line="83"/>
         <source>Language</source>
-        <translation>мова</translation>
+        <translation>Мова</translation>
     </message>
     <message id="id-bluetooth-page">
         <location filename="../src/qml/main.qml" line="89"/>
@@ -110,7 +110,7 @@
     <message id="id-always-on-display">
         <location filename="../src/qml/DisplayPage.qml" line="124"/>
         <source>Always on Display</source>
-        <translation>Завжди на дисплеї</translation>
+        <translation>AOD</translation>
     </message>
     <message id="id-burn-in-protection">
         <location filename="../src/qml/DisplayPage.qml" line="140"/>
@@ -120,29 +120,29 @@
     <message id="id-tilt-to-wake">
         <location filename="../src/qml/DisplayPage.qml" line="158"/>
         <source>Tilt-to-wake</source>
-        <translation>Пробудження за нахилом</translation>
+        <translation>Пробудження нахилом</translation>
     </message>
     <message id="id-tap-to-wake">
         <location filename="../src/qml/DisplayPage.qml" line="177"/>
         <source>Tap-to-wake</source>
-        <translation>Пробудження за дотиком</translation>
+        <translation>Пробудження дотиком</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../src/qml/main.qml" line="101"/>
         <source>Sound</source>
-        <translation>звук</translation>
+        <translation>Звук</translation>
     </message>
     <message id="id-nightstand-page">
         <location filename="../src/qml/main.qml" line="108"/>
         <location filename="../src/qml/NightstandPage.qml" line="49"/>
         <source>Nightstand</source>
-        <translation type="unfinished"></translation>
+        <translation>Нічний вид</translation>
     </message>
     <message id="id-units-page">
         <location filename="../src/qml/main.qml" line="114"/>
         <location filename="../src/qml/UnitsPage.qml" line="26"/>
         <source>Units</source>
-        <translation>одиниці</translation>
+        <translation>Одиниці</translation>
     </message>
     <message id="id-wallpaper-page">
         <location filename="../src/qml/main.qml" line="120"/>
@@ -157,7 +157,7 @@
     <message id="id-launcher-page">
         <location filename="../src/qml/main.qml" line="132"/>
         <source>Launcher</source>
-        <translation>Запускач</translation>
+        <translation>Лаунчер</translation>
     </message>
     <message id="id-usb-page">
         <location filename="../src/qml/main.qml" line="138"/>
@@ -178,7 +178,7 @@
     <message id="id-about-page">
         <location filename="../src/qml/main.qml" line="156"/>
         <source>About</source>
-        <translation>Про ПЗ</translation>
+        <translation>Про</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-settings.desktop.h" line="6"/>
@@ -188,25 +188,25 @@
     <message id="id-nightstand-enable">
         <location filename="../src/qml/NightstandPage.qml" line="64"/>
         <source>Enable</source>
-        <translation type="unfinished"></translation>
+        <translation>Увімкнути</translation>
     </message>
     <message id="id-nightstand-brightness">
         <location filename="../src/qml/NightstandPage.qml" line="77"/>
         <source>Brightness</source>
         <oldsource>Nightstand Brightness</oldsource>
-        <translation type="unfinished">Яскравість</translation>
+        <translation>Яскравість</translation>
     </message>
     <message id="id-nightstand-delay">
         <location filename="../src/qml/NightstandPage.qml" line="97"/>
         <source>Delay</source>
         <oldsource>Nightstand delay</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Затримка</translation>
     </message>
     <message id="id-nightstand-watchface">
         <location filename="../src/qml/NightstandPage.qml" line="134"/>
         <source>Select watchface</source>
         <oldsource>select nightstand watchface</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Виберіть циферблат</translation>
     </message>
 </context>
 <context>
@@ -214,73 +214,73 @@
     <message>
         <location filename="../src/qml/AboutPage.qml" line="59"/>
         <source>Build ID</source>
-        <translation type="unfinished"></translation>
+        <translation>ID збірки</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="60"/>
         <source>Codename</source>
-        <translation type="unfinished"></translation>
+        <translation>Кодова назва</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="61"/>
         <source>Host name</source>
-        <translation type="unfinished"></translation>
+        <translation>Ім&apos;я хоста</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="62"/>
         <source>WLAN MAC</source>
-        <translation type="unfinished"></translation>
+        <translation>WLAN MAC</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="63"/>
         <source>IMEI</source>
-        <translation type="unfinished"></translation>
+        <translation>IMEI</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="64"/>
         <source>Serial number</source>
-        <translation type="unfinished"></translation>
+        <translation>Серійний номер</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="65"/>
         <source>Total disk space</source>
-        <translation type="unfinished"></translation>
+        <translation>Всього місця на диску</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="65"/>
         <location filename="../src/qml/AboutPage.qml" line="66"/>
         <source>%L1</source>
-        <translation type="unfinished"></translation>
+        <translation>%L1</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="66"/>
         <source>Available disk space</source>
-        <translation type="unfinished"></translation>
+        <translation>Доступне місце на диску</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="68"/>
         <source>Display size</source>
-        <translation type="unfinished"></translation>
+        <translation>Розмір дисплея</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="68"/>
         <source>W</source>
-        <translation type="unfinished"></translation>
+        <translation>W</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="68"/>
         <source>H</source>
-        <translation type="unfinished"></translation>
+        <translation>H</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="69"/>
         <source>Kernel version</source>
-        <translation type="unfinished"></translation>
+        <translation>Версія ядра</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="70"/>
         <source>Qt version</source>
-        <translation type="unfinished"></translation>
+        <translation>Версія Qt</translation>
     </message>
 </context>
 </TS>

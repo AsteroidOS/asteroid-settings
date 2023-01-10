e9f9fa45a89c562514e0b85c1e1363c0f645df2e
Xyngg <heyabhinav@protonmail.com>
2023-01-10 16:55:25 +0000

    Translated using Weblate (Hindi)
    Currently translated at 94.3% (50 of 53 strings)

--- i18n/asteroid-settings.hi.ts
+++ i18n/asteroid-settings.hi.ts
@@ -6,207 +6,207 @@
     <message id="id-bluetooth-on">
         <location filename="../src/qml/BluetoothPage.qml" line="28"/>
         <source>Bluetooth on</source>
-        <translation type="unfinished"></translation>
+        <translation>ब्लूटूथ चालू है</translation>
     </message>
     <message id="id-bluetooth-off">
         <location filename="../src/qml/BluetoothPage.qml" line="30"/>
         <source>Bluetooth off</source>
-        <translation type="unfinished"></translation>
+        <translation>ब्लूटूथ बंद है</translation>
     </message>
     <message id="id-connected">
         <location filename="../src/qml/BluetoothPage.qml" line="32"/>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>कनेक्टेड</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../src/qml/BluetoothPage.qml" line="34"/>
         <source>Not connected</source>
-        <translation type="unfinished"></translation>
+        <translation>जुड़ा नहीं है</translation>
     </message>
     <message id="id-poweroff-warn">
         <location filename="../src/qml/PoweroffPage.qml" line="29"/>
         <source>Power off AsteroidOS</source>
-        <translation type="unfinished"></translation>
+        <translation>एस्टेरोइड ओ. एस. को पावर ऑफ करें</translation>
     </message>
     <message id="id-reboot-warn">
         <location filename="../src/qml/RebootPage.qml" line="29"/>
         <source>Reboot AsteroidOS</source>
-        <translation type="unfinished"></translation>
+        <translation>एस्टेरोइड ओ. एस. रीबूट करें</translation>
     </message>
     <message id="id-sound-percentage">
         <location filename="../src/qml/SoundPage.qml" line="36"/>
         <source>Volume %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>वॉल्यूम %1%</translation>
     </message>
     <message id="id-charging-only">
         <location filename="../src/qml/USBPage.qml" line="35"/>
         <source>Charging only</source>
-        <translation type="unfinished"></translation>
+        <translation>केवल चार्ज हो रहा है</translation>
     </message>
     <message id="id-adb-mode">
         <location filename="../src/qml/USBPage.qml" line="37"/>
         <source>ADB Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>ए डी बी मोड</translation>
     </message>
     <message id="id-developer-mode">
         <location filename="../src/qml/USBPage.qml" line="39"/>
         <source>Developer Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>डेवलपर मोड</translation>
     </message>
     <message id="id-mtp-mode">
         <location filename="../src/qml/USBPage.qml" line="41"/>
         <source>MTP Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>एम टी पी मोड</translation>
     </message>
     <message id="id-12h-format">
         <location filename="../src/qml/UnitsPage.qml" line="48"/>
         <source>Use 12H format:</source>
-        <translation type="unfinished"></translation>
+        <translation>12 एच फॉर्मेट का प्रयोग करो:</translation>
     </message>
     <message id="id-fahrenheit">
         <location filename="../src/qml/UnitsPage.qml" line="63"/>
         <source>Use Fahrenheit:</source>
-        <translation type="unfinished"></translation>
+        <translation>फारेनहाइट का प्रयोग करो</translation>
     </message>
     <message id="id-time-page">
         <location filename="../src/qml/main.qml" line="71"/>
         <location filename="../src/qml/TimePage.qml" line="40"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>समय</translation>
     </message>
     <message id="id-date-page">
         <location filename="../src/qml/DatePage.qml" line="31"/>
         <location filename="../src/qml/main.qml" line="77"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>तारीख</translation>
     </message>
     <message id="id-language-page">
         <location filename="../src/qml/LanguagePage.qml" line="31"/>
         <location filename="../src/qml/main.qml" line="83"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>भाषा</translation>
     </message>
     <message id="id-bluetooth-page">
         <location filename="../src/qml/main.qml" line="89"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>ब्लूटूथ</translation>
     </message>
     <message id="id-display-page">
         <location filename="../src/qml/DisplayPage.qml" line="39"/>
         <location filename="../src/qml/main.qml" line="95"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>डिस्प्ले</translation>
     </message>
     <message id="id-brightness">
         <location filename="../src/qml/DisplayPage.qml" line="61"/>
         <source>Brightness</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">चमक</translation>
     </message>
     <message id="id-automatic-brightness">
         <location filename="../src/qml/DisplayPage.qml" line="108"/>
         <source>Automatic brightness</source>
-        <translation type="unfinished"></translation>
+        <translation>स्वचालित चमक</translation>
     </message>
     <message id="id-always-on-display">
         <location filename="../src/qml/DisplayPage.qml" line="124"/>
         <source>Always on Display</source>
-        <translation type="unfinished"></translation>
+        <translation>हमेशा डिस्प्ले पर</translation>
     </message>
     <message id="id-burn-in-protection">
         <location filename="../src/qml/DisplayPage.qml" line="140"/>
         <source>Burn in protection</source>
-        <translation type="unfinished"></translation>
+        <translation>डिस्प्ले जलने का संरक्षण बचाव</translation>
     </message>
     <message id="id-tilt-to-wake">
         <location filename="../src/qml/DisplayPage.qml" line="158"/>
         <source>Tilt-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>टिल्ट-टु-वेक</translation>
     </message>
     <message id="id-tap-to-wake">
         <location filename="../src/qml/DisplayPage.qml" line="177"/>
         <source>Tap-to-wake</source>
-        <translation type="unfinished"></translation>
+        <translation>टैप-टु-वेक</translation>
     </message>
     <message id="id-sound-page">
         <location filename="../src/qml/main.qml" line="101"/>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>ध्वनि</translation>
     </message>
     <message id="id-nightstand-page">
         <location filename="../src/qml/main.qml" line="108"/>
         <location filename="../src/qml/NightstandPage.qml" line="49"/>
         <source>Nightstand</source>
-        <translation type="unfinished"></translation>
+        <translation>रात्रिस्तंभ</translation>
     </message>
     <message id="id-units-page">
         <location filename="../src/qml/main.qml" line="114"/>
         <location filename="../src/qml/UnitsPage.qml" line="26"/>
         <source>Units</source>
-        <translation type="unfinished"></translation>
+        <translation>यूनिट</translation>
     </message>
     <message id="id-wallpaper-page">
         <location filename="../src/qml/main.qml" line="120"/>
         <source>Wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation>वॉलपेपर</translation>
     </message>
     <message id="id-watchface-page">
         <location filename="../src/qml/main.qml" line="126"/>
         <source>Watchface</source>
-        <translation type="unfinished"></translation>
+        <translation>वॉचफेस</translation>
     </message>
     <message id="id-launcher-page">
         <location filename="../src/qml/main.qml" line="132"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation>लांचर</translation>
     </message>
     <message id="id-usb-page">
         <location filename="../src/qml/main.qml" line="138"/>
         <location filename="../src/qml/USBPage.qml" line="29"/>
         <source>USB</source>
-        <translation type="unfinished"></translation>
+        <translation>यु एस बी</translation>
     </message>
     <message id="id-poweroff-page">
         <location filename="../src/qml/main.qml" line="144"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>बिजली बंद है</translation>
     </message>
     <message id="id-reboot-page">
         <location filename="../src/qml/main.qml" line="150"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>रीबूट</translation>
     </message>
     <message id="id-about-page">
         <location filename="../src/qml/main.qml" line="156"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>एस्टेरोइड ओ. एस. के बारे में</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-settings.desktop.h" line="6"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>सेटिंग्स</translation>
     </message>
     <message id="id-nightstand-enable">
         <location filename="../src/qml/NightstandPage.qml" line="64"/>
         <source>Enable</source>
-        <translation type="unfinished"></translation>
+        <translation>चालून करे</translation>
     </message>
     <message id="id-nightstand-brightness">
         <location filename="../src/qml/NightstandPage.qml" line="77"/>
         <source>Brightness</source>
         <oldsource>Nightstand Brightness</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>चमक</translation>
     </message>
     <message id="id-nightstand-delay">
         <location filename="../src/qml/NightstandPage.qml" line="97"/>
         <source>Delay</source>
         <oldsource>Nightstand delay</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>देरी</translation>
     </message>
     <message id="id-nightstand-watchface">
         <location filename="../src/qml/NightstandPage.qml" line="134"/>
         <source>Select watchface</source>
         <oldsource>select nightstand watchface</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>वॉचफेस चुनें</translation>
     </message>
 </context>
 <context>
@@ -214,53 +214,53 @@
     <message>
         <location filename="../src/qml/AboutPage.qml" line="59"/>
         <source>Build ID</source>
-        <translation type="unfinished"></translation>
+        <translation>बिल्ड आई डी</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="60"/>
         <source>Codename</source>
-        <translation type="unfinished"></translation>
+        <translation>कोड नाम</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="61"/>
         <source>Host name</source>
-        <translation type="unfinished"></translation>
+        <translation>होस्ट  का नाम</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="62"/>
         <source>WLAN MAC</source>
-        <translation type="unfinished"></translation>
+        <translation>व्लान लैन</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="63"/>
         <source>IMEI</source>
-        <translation type="unfinished"></translation>
+        <translation>आई एम ई आई</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="64"/>
         <source>Serial number</source>
-        <translation type="unfinished"></translation>
+        <translation>सीरियल नंबर</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="65"/>
         <source>Total disk space</source>
-        <translation type="unfinished"></translation>
+        <translation>डिस्क का कुल जगह</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="65"/>
         <location filename="../src/qml/AboutPage.qml" line="66"/>
         <source>%L1</source>
-        <translation type="unfinished"></translation>
+        <translation>%L1</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="66"/>
         <source>Available disk space</source>
-        <translation type="unfinished"></translation>
+        <translation>डिस्क का उपलब्ध जगह</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="68"/>
         <source>Display size</source>
-        <translation type="unfinished"></translation>
+        <translation>प्रदर्शन का आकार</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="68"/>
@@ -275,12 +275,12 @@
     <message>
         <location filename="../src/qml/AboutPage.qml" line="69"/>
         <source>Kernel version</source>
-        <translation type="unfinished"></translation>
+        <translation>कर्नेल वर्जन</translation>
     </message>
     <message>
         <location filename="../src/qml/AboutPage.qml" line="70"/>
         <source>Qt version</source>
-        <translation type="unfinished"></translation>
+        <translation>क्यू टी वर्जन</translation>
     </message>
 </context>
 </TS>

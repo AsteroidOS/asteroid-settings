b229b4ee2a28470bb5b249005bfc93f39c671fe9
bittin1ddc447d824349b2 <bittin@reimu.nl>
2023-05-11 09:34:01 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (64 of 64 strings)

--- i18n/asteroid-settings.sv.ts
+++ i18n/asteroid-settings.sv.ts
@@ -27,7 +27,7 @@
         <location filename="../src/qml/SoundPage.qml" line="88"/>
         <source>Volume</source>
         <oldsource>Volume %1%</oldsource>
-        <translation type="unfinished">Volym %1%</translation>
+        <translation>Volym</translation>
     </message>
     <message id="id-charging-only">
         <location filename="../src/qml/USBPage.qml" line="30"/>

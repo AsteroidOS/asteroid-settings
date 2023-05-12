63973ba471b8978be34b8989af748cb27abafd0f
Andi Chandler <andi@gowling.com>
2023-05-12 20:21:54 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (64 of 64 strings)

--- i18n/asteroid-settings.en_GB.ts
+++ i18n/asteroid-settings.en_GB.ts
@@ -27,7 +27,7 @@
         <location filename="../src/qml/SoundPage.qml" line="88"/>
         <source>Volume</source>
         <oldsource>Volume %1%</oldsource>
-        <translation type="unfinished">Volume %1%</translation>
+        <translation>Volume</translation>
     </message>
     <message id="id-charging-only">
         <location filename="../src/qml/USBPage.qml" line="30"/>
